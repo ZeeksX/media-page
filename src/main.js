7f5369c95eaff397bf2e99d10b8f5da9fe0de04b
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-30 12:53:06 +0100

    changes

--- src/main.js
+++ src/main.js
@@ -2,5 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
-
+import "../public/index.css"
 createApp(App).use(router).use(createPinia()).mount('#app')
