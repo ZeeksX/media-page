0c9a703df8178930a4271507b47351c5bcc78777
ZeeksX <ikinwotezekiel@gmail.com>
2024-05-30 15:23:25 +0100

    changes

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore('counter', {
-  state: () => ({ count: 0 }),
-  getters: {
-    double: state => state.count * 2,
-  },
-  actions: {
-    increment() {
-      this.count++
-    },
-  },
-})
+export const useCounterStore = defineStore("counter", {
+	state: () => ({
+		snippet: [],
+	}),
+	getters: {},
+	actions: {
+		async fetchData() {
+			try {
+				const data = await fetch(
+					"https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=UUzsMXDElPmWUht3mfQOJBkg&key=AIzaSyB6mPE6M8cF2DlZ1JPDd3Hag6Ybwlmnfmg "
+				);
+				const res = await data.json();
+				this.snippet = res.items;
+			} catch (error) {
+				console.warn(error);
+			}
+      console.log(this.snippet)
+		},
+	},
+});
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -21,133 +21,46 @@
         </div>
         <div class="video-frame">
             <div class="loader">
-             
+                <img v-show="loading" src="@/assets/loader.svg" alt="loading" />
+                <div class="video-clip"></div>
             </div>
+        </div>
+        <div class="related-videos">
+            <div class="other"></div>
         </div>
     </div>
 </template>
-<style>
-.video-frame {
-    max-width: 64rem;
-    margin: 0 auto 4.125rem;
-    display: flex;
-    justify-content: center;
-    height: 36rem;
-    border-radius: 21.114px;
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.10) 0%, rgba(0, 0, 0, 0.10) 100%), url("../assets/loader.svg") lightgray 50% / cover no-repeat;
-}
-.loader{
-    background-color: #bebebe;
-}
-.evolving {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-}
-
-.evolving h1 {
-    width: 64.5%;
-    padding: 0.25px;
-    margin: 0 16rem 1rem;
-    text-align: center;
-    font-size: 48px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: 59.5px;
-    letter-spacing: -1.92px;
-    background: var(--Bg-1, linear-gradient(180deg, #1A2837 0%, #031A05 100%));
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-}
-
-.evolving p {
-    font-size: 18px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 150%;
-    width: 40%;
-    text-align: center;
-    opacity: 0.7;
-    background: var(--Bg-1, linear-gradient(180deg, #1A2837 0%, #031A05 100%));
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    margin: 0 auto 7.375rem;
-}
-
-.home {
-    padding: 0;
-    margin: 0;
-}
-
-#logo {
-    width: 100%;
-}
-
-.navbar {
-    margin: 5.125rem auto 8rem;
-    display: flex;
-    width: 70%;
+<style></style>
+<script>
+import { useCounterStore } from "@/store/index.js"
+export default {
+    data() {
+        return {
+            loading: true,
+            counterStore: useCounterStore(),
+            videoArray: useCounterStore().snippet
+        }
+    },
+    async mounted() {
+        await this.counterStore.fetchData();
+        setTimeout(this.showVideos, 3000);
+    },
+    methods: {
+        showVideos() {
+            this.playVideo();
+            this.loading = false;
+        },
+        playVideo() {
+            const video = document.querySelector(".video-clip")
+            this.videoArray.forEach(element => {
+                video.innerHTML += `
+                <a href="https://www.youtube.com/watch?v=${element.resourceId.videoId}">
+                 <img src=${element.thumbnails.maxres.url}/>
+                </a> 
+            `
+            });
+        },
+    }
 
 }
-
-#signin {
-    border-radius: 5.41px;
-    border: 0.815px solid rgba(7, 48, 66, 0.20);
-    background: #FFF;
-    color: #073042;
-    width: 110px;
-    height: 44px;
-}
-
-#signup {
-    border-radius: 5.41px;
-    background: #26C165;
-    border-color: unset;
-    width: 110px;
-    height: 44px;
-}
-
-.list-content-one {
-    display: flex;
-    gap: 5rem;
-    align-items: center;
-}
-
-.list-content-two {
-    display: flex;
-    align-items: center;
-
-}
-
-.list-items {
-    margin: 0;
-    padding: 0;
-    display: flex;
-    flex-direction: row;
-    gap: 4rem;
-    justify-content: center;
-    align-items: center;
-}
-
-li {
-    list-style-type: none;
-    font-size: 14.667px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 80%;
-    /* 11.733px */
-    letter-spacing: -0.587px;
-}
-
-a {
-    text-decoration: none;
-    color: black;
-}
-
-a:hover {
-    cursor: pointer;
-}
-</style>
+</script>
